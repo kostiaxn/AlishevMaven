aaed9f0e8775788e312494e5ecaa9dda293252b5
kosti <kostia.xn@gmail.com>
2023-04-27 17:47:56 +0300

    JUnit

--- src/main/java/MyMath.java
+++ src/main/java/MyMath.java
@@ -1,2 +1,7 @@
-package PACKAGE_NAME;public class MyMath {
+public class MyMath {
+    public static double divide(int number1, int number2){
+        if(number2 == 0)
+            throw new ArithmeticException("Can't divide by zero");
+        return number1 / number2;
+    }
 }
--- src/test/java/MyMathTest.java
+++ src/test/java/MyMathTest.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class MyMathTest {
+import org.junit.Test;
+
+public class MyMathTest {
+    @Test(expected = ArithmeticException.class)
+    public void zeroDenominatorShouldThrowException(){
+        MyMath.divide(1,0);
+    }
 }
