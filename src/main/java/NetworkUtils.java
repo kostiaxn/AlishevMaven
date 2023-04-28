0aca4390337fa1107e7717fcb7f75a46ef5a8187
kosti <kostia.xn@gmail.com>
2023-04-28 13:02:52 +0300

    JUnit

--- src/main/java/NetworkUtils.java
+++ src/main/java/NetworkUtils.java
@@ -2,7 +2,7 @@ public class NetworkUtils {
     public static void getConnection(){
         // получаем соединение с сервером
         try {
-            Thread.sleep(1500);
+            Thread.sleep(1);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
